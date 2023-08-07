8ac67ca8f8dc11b0431b8a5e6ac034088bcf7017
Jack Szwergold <JackSzwergold@gmail.com>
2023-08-07 14:51:53 -0400

    Some hamfisted work to get cropping in place.

--- script/script.js
+++ script/script.js
@@ -21,7 +21,6 @@ resetFilterButton = document.querySelector("#reset_filter");
 chooseImageButton = document.querySelector("#choose_image");
 saveImageButton = document.querySelector("#save_image");
 
-
 let brightness = 100,
     saturation = 100, 
     grayscale = 0
@@ -32,7 +31,17 @@ let rotate = 0,
     flipVertical = 1
     ;
 
+let event_state = {},
+    constrain = false,
+      min_width = 60,
+      min_height = 60,
+      max_width = 800,
+      max_height = 900,
+      resize_canvas = document.createElement('canvas');
+    ;
+
 const loadImage = () => {
+
     let file = fileInput.files[0];
     if (!file) return;
     previewImage.src = URL.createObjectURL(file);
@@ -40,6 +49,19 @@ const loadImage = () => {
     previewImage.addEventListener("load", () => {
         resetFilterButton.click();
     });
+
+    $(previewImage).wrap('<div class="resize-container"></div>')
+    .before('<span class="resize-handle resize-handle-nw"></span>')
+    .before('<span class="resize-handle resize-handle-ne"></span>')
+    .after('<span class="resize-handle resize-handle-se"></span>')
+    .after('<span class="resize-handle resize-handle-sw"></span>')
+    ;
+
+    $container =  $(previewImage).parent('.resize-container');
+
+    $container.on('mousedown touchstart', '.resize-handle', startResize);
+    $container.on('mousedown touchstart', 'img', startMoving);
+
 }
 
 const applyFilter = () => {
@@ -66,7 +88,6 @@ const updateFilter = () => {
 }
 
 rotateOptions.forEach(option => {
-
 
     option.addEventListener("click", () => {
         if (option.id === "left") {
@@ -95,6 +116,7 @@ rotateOptions.forEach(option => {
         applyFilter();
 
     });
+
 });
 
 const resetFilter = () => {
@@ -156,6 +178,146 @@ const saveImage = () => {
     link.download = fileName;
     link.href = canvas.toDataURL('image/jpeg', 0.75);
     link.click();
+}
+
+const resizeImage = (width, height) => {
+    resize_canvas.width = width;
+    resize_canvas.height = height;
+    resize_canvas.getContext('2d').drawImage(previewImage, 0, 0, width, height);   
+    $(previewImage).attr('src', resize_canvas.toDataURL("image/png"));  
+}
+
+const startResize = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    saveEventState(e);
+    $(document).on('mousemove touchmove', resizing);
+    $(document).on('mouseup touchend', endResize);
+};
+
+const endResize = (e) => {
+    e.preventDefault();
+    $(document).off('mouseup touchend', endResize);
+    $(document).off('mousemove touchmove', resizing);
+};
+
+const saveEventState = (e) => {
+
+    event_state.container_width = $container.width();
+    event_state.container_height = $container.height();
+    event_state.container_left = $container.offset().left; 
+    event_state.container_top = $container.offset().top;
+    event_state.mouse_x = (e.clientX || e.pageX || e.originalEvent.touches[0].clientX) + $(window).scrollLeft(); 
+    event_state.mouse_y = (e.clientY || e.pageY || e.originalEvent.touches[0].clientY) + $(window).scrollTop();
+    
+    if(typeof e.originalEvent.touches !== 'undefined') {
+        event_state.touches = [];
+        $.each(e.originalEvent.touches, function(i, ob) {
+          event_state.touches[i] = {};
+          event_state.touches[i].clientX = 0+ob.clientX;
+          event_state.touches[i].clientY = 0+ob.clientY;
+        });
+    }
+
+    event_state.evnt = e;
+
+};
+
+const resizing = (e) => {
+    var mouse={},width,height,left,top,offset=$container.offset();
+    mouse.x = (e.clientX || e.pageX || e.originalEvent.touches[0].clientX) + $(window).scrollLeft(); 
+    mouse.y = (e.clientY || e.pageY || e.originalEvent.touches[0].clientY) + $(window).scrollTop();
+
+    if( $(event_state.evnt.target).hasClass('resize-handle-se') ){
+      width = mouse.x - event_state.container_left;
+      height = mouse.y  - event_state.container_top;
+      left = event_state.container_left;
+      top = event_state.container_top;
+    }
+    else if ($(event_state.evnt.target).hasClass('resize-handle-sw') ){
+      width = event_state.container_width - (mouse.x - event_state.container_left);
+      height = mouse.y  - event_state.container_top;
+      left = mouse.x;
+      top = event_state.container_top;
+    }
+    else if ($(event_state.evnt.target).hasClass('resize-handle-nw') ){
+      width = event_state.container_width - (mouse.x - event_state.container_left);
+      height = event_state.container_height - (mouse.y - event_state.container_top);
+      left = mouse.x;
+      top = mouse.y;
+      if(constrain || e.shiftKey){
+        top = mouse.y - ((width / previewImage.width * previewImage.height) - height);
+      }
+    }
+    else if ($(event_state.evnt.target).hasClass('resize-handle-ne') ){
+      width = mouse.x - event_state.container_left;
+      height = event_state.container_height - (mouse.y - event_state.container_top);
+      left = event_state.container_left;
+      top = mouse.y;
+      if(constrain || e.shiftKey){
+        top = mouse.y - ((width / previewImage.width * previewImage.height) - height);
+      }
+    }
+
+    if (constrain || e.shiftKey){
+      height = width / previewImage.width * previewImage.height;
+    }
+
+    if (width > min_width && height > min_height && width < max_width && height < max_height){
+      resizeImage(width, height);  
+      $container.offset({'left': left, 'top': top});
+    }
+}
+
+const startMoving = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    saveEventState(e);
+    $(document).on('mousemove touchmove', moving);
+    $(document).on('mouseup touchend', endMoving);
+}
+
+const endMoving = (e) => {
+    e.preventDefault();
+    $(document).off('mouseup touchend', endMoving);
+    $(document).off('mousemove touchmove', moving);
+}
+
+const moving = (e) => {
+    var mouse = {}, touches;
+    e.preventDefault();
+    e.stopPropagation();
+    
+    touches = e.originalEvent.touches;
+    
+    mouse.x = (e.clientX || e.pageX || touches[0].clientX) + $(window).scrollLeft(); 
+    mouse.y = (e.clientY || e.pageY || touches[0].clientY) + $(window).scrollTop();
+    $container.offset({
+      'left': mouse.x - ( event_state.mouse_x - event_state.container_left ),
+      'top': mouse.y - ( event_state.mouse_y - event_state.container_top ) 
+    });
+
+    if (event_state.touches && event_state.touches.length > 1 && touches.length > 1) {
+      var width = event_state.container_width, height = event_state.container_height;
+      var a = event_state.touches[0].clientX - event_state.touches[1].clientX;
+      a = a * a; 
+      var b = event_state.touches[0].clientY - event_state.touches[1].clientY;
+      b = b * b; 
+      var dist1 = Math.sqrt( a + b );
+      
+      a = e.originalEvent.touches[0].clientX - touches[1].clientX;
+      a = a * a; 
+      b = e.originalEvent.touches[0].clientY - touches[1].clientY;
+      b = b * b; 
+      var dist2 = Math.sqrt( a + b );
+
+      var ratio = dist2 /dist1;
+
+      width = width * ratio;
+      height = height * ratio;
+
+      resizeImage(width, height);
+    }
 }
 
 filterSliderBrightness.addEventListener("input", updateFilter);
