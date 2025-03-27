3f2c96ebb17a1d50add0e3b7e5eea604afe5f767
Jack Szwergold <JackSzwergold@gmail.com>
2025-03-27 19:57:25 -0400

    More work.

--- test_receiver.php
+++ test_receiver.php
@@ -1,8 +1,9 @@
 <?php
 
-$backGround = substr($backGround, 22);
-$backGround = str_replace(' ', '+', $backGround);
-$backGround = base64_decode($backGround);
-file_put_contents('test.jpg', $backGround);
+$data = $_POST['data'];
+$data = substr($data, strpos($data, ",") + 1);
+$data = base64_decode($data);
+$file = 'test.jpg';
+file_put_contents($file, $data);
 
 ?>
