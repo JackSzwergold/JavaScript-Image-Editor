d980648c6515b4e6d03ca138e4378440c6573a06
Jack Szwergold <JackSzwergold@gmail.com>
2025-03-27 20:19:25 -0400

    More work.

--- receiver.php
+++ receiver.php
@@ -6,6 +6,12 @@ $filename = $_POST['filename'];
 $data = substr($data, strpos($data, ",") + 1);
 $data = base64_decode($data);
 
+$md5_full = md5($data);
+$md5_short = substr($md5_full, 0, 7);
+
+$filename = $md5_short . '_' . $filename;
+
 file_put_contents($filename, $data);
+
 
 ?>
